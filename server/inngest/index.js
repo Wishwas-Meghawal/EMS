cf641da982dc71fe09e82c662f3860ae5940afeb
Wishwas <wishwasmeghawal1441@gmail.com>
2026-06-19 09:06:01 +0530

    fixed emailsent corn for attendance

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -302,10 +302,11 @@ const attendanceReminderCron = inngest.createFunction(
                   `
               })
             });
+            await Promise.all(emailPromises);
+            return {emailSent: absentEmployees.length};
         });
     }
 
-     await Promise.all(emailPromises);
 
     return { totalActive: activeEmployees.length, onLeave: onLeaveIds.length, checkedIn: checkedInIds.length, absent: absentEmployees.length }
 
